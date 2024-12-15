7db3a008dba43d4547a296d4cf30530b4837f802
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2024-12-15 16:16:18 +0100

    Add LICENSE and README

--- GUI.java
+++ GUI.java
@@ -1,3 +1,21 @@
+/*
+    TicTacToeAI
+    Copyright (C) 2024 Jochengehtab
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
@@ -110,7 +128,7 @@ public class GUI extends JFrame {
         while (!board.isFull() && !board.isGameOver()) {
 
             // Get the bestmove
-            int[] bestMove = search.getBestMove(board, 6);
+            int[] bestMove = search.getBestMove(board, 8);
 
             // Make the move
             board.makeMove(bestMove);
