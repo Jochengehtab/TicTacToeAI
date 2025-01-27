e27d93b465007fdcbb015405422d1d0c0b569cda
Jochen <97750753+Jochengehtab@users.noreply.github.com>
2025-01-27 18:22:41 +0100

    Re-License under AGPL

--- src/GameManager/GameManager.java
+++ src/GameManager/GameManager.java
@@ -1,19 +1,20 @@
 /*
-    TicTacToeAI
-    Copyright (C) 2024 Jochengehtab
+  This file is part of the TicTacToe AI written by Jochengehtab
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  Copyright (C) 2024-2025 Jochengehtab
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 package src.GameManager;
